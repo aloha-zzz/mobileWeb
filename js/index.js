65fb2c8e179bfb31e877359c5ad2b901c69d5559
zhou haoxiang <835390899@qq.com>
2017-06-01 14:48:17 +0800

    writing

---
+++ js/index.js
@@ -0,0 +1,7 @@
+var screenWidth=document.body.clientWidth;
+var screenHeight=document.body.clientHeight;
+for(var i=0;i<11;i++){
+	document.getElementsByClassName('swiper-slide')[i].style.width=screenWidth+'px';
+	document.getElementsByClassName('swiper-slide')[i].style.height=screenHeight+'px';
+}
+document.documentElement.style.fontSize = document.documentElement.clientWidth / 6.4 + 'px';
