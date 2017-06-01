01d6ef923370071bbf67c8020850a1573c1aa35b
zhou haoxiang <835390899@qq.com>
2017-06-01 21:14:29 +0800

    coding

--- js/index.js
+++ js/index.js
@@ -5,12 +5,34 @@
 // 	document.getElementsByClassName('swiper-slide')[i].style.height=screenHeight+'px';
 // }
 document.documentElement.style.fontSize = document.documentElement.clientWidth / 6.4 + 'px';
+var screenWidth=document.body.clientWidth;
+var screenHeight=document.body.clientHeight;
 window.onresize=function init() {
-	var screenWidth=document.body.clientWidth;
-	var screenHeight=document.body.clientHeight;
+	screenWidth=document.body.clientWidth;
+	screenHeight=document.body.clientHeight;
 	for(var i=0;i<11;i++){
 		document.getElementsByClassName('swiper-slide')[i].style.width=screenWidth+'px';
 		document.getElementsByClassName('swiper-slide')[i].style.height=screenHeight+'px';
 	}
 	document.documentElement.style.fontSize = document.documentElement.clientWidth / 6.4 + 'px';
 }
+
+setInterval(function () {
+    var height= $("#slide1").offset().top;
+    if(height==0){
+
+	}else if(height==(-screenHeight)){
+		$("#playintwo").addClass("fadeInDownShort");
+	}else if(height==(-screenHeight*2)){
+        $("#playinthree").addClass("fadeInUpShort");
+	}else if(height==(-screenHeight*3)){
+        $("#playinfour").addClass("fadeInUpShort");
+        $("#sun").addClass("tada");
+	}else if(height==(-screenHeight*5)){
+        $("#playinsix").addClass("fadeInUpShort");
+        $("#down").addClass("fadeInDown");
+        $("#word").addClass("fadeIn");
+    }else if(height==(-screenHeight*10)){
+		$("#playineight").addClass("fadeInUpShort");
+	}
+},500)
