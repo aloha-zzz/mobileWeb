a7be1bbd299ff067df3c45b42f36942add46910f
zhou haoxiang <835390899@qq.com>
2017-06-01 15:35:28 +0800

    coding

--- js/index.js
+++ js/index.js
@@ -5,3 +5,12 @@ for(var i=0;i<11;i++){
 	document.getElementsByClassName('swiper-slide')[i].style.height=screenHeight+'px';
 }
 document.documentElement.style.fontSize = document.documentElement.clientWidth / 6.4 + 'px';
+window.onresize=function init() {
+	var screenWidth=document.body.clientWidth;
+var screenHeight=document.body.clientHeight;
+for(var i=0;i<11;i++){
+	document.getElementsByClassName('swiper-slide')[i].style.width=screenWidth+'px';
+	document.getElementsByClassName('swiper-slide')[i].style.height=screenHeight+'px';
+}
+document.documentElement.style.fontSize = document.documentElement.clientWidth / 6.4 + 'px';
+}
