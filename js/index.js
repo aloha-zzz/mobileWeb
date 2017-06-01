7f36c86d8c9baa07685bf41213a1f4051ca73e1c
zhou haoxiang <835390899@qq.com>
2017-06-01 15:56:40 +0800

    coding

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
-var screenWidth=document.body.clientWidth;
-var screenHeight=document.body.clientHeight;
-for(var i=0;i<11;i++){
-	document.getElementsByClassName('swiper-slide')[i].style.width=screenWidth+'px';
-	document.getElementsByClassName('swiper-slide')[i].style.height=screenHeight+'px';
-}
+// var screenWidth=document.body.clientWidth;
+// var screenHeight=document.body.clientHeight;
+// for(var i=0;i<11;i++){
+// 	document.getElementsByClassName('swiper-slide')[i].style.width=screenWidth+'px';
+// 	document.getElementsByClassName('swiper-slide')[i].style.height=screenHeight+'px';
+// }
 document.documentElement.style.fontSize = document.documentElement.clientWidth / 6.4 + 'px';
 window.onresize=function init() {
 	var screenWidth=document.body.clientWidth;
-var screenHeight=document.body.clientHeight;
-for(var i=0;i<11;i++){
-	document.getElementsByClassName('swiper-slide')[i].style.width=screenWidth+'px';
-	document.getElementsByClassName('swiper-slide')[i].style.height=screenHeight+'px';
-}
-document.documentElement.style.fontSize = document.documentElement.clientWidth / 6.4 + 'px';
+	var screenHeight=document.body.clientHeight;
+	for(var i=0;i<11;i++){
+		document.getElementsByClassName('swiper-slide')[i].style.width=screenWidth+'px';
+		document.getElementsByClassName('swiper-slide')[i].style.height=screenHeight+'px';
+	}
+	document.documentElement.style.fontSize = document.documentElement.clientWidth / 6.4 + 'px';
 }
