02798a438854c9604910431007ee0aa8775bb471
zhou haoxiang <835390899@qq.com>
2017-06-03 22:22:38 +0800

    外包：山东大学信息检索实验室招生移动端页面

--- js/index.js
+++ js/index.js
@@ -4,22 +4,40 @@
 // 	document.getElementsByClassName('swiper-slide')[i].style.width=screenWidth+'px';
 // 	document.getElementsByClassName('swiper-slide')[i].style.height=screenHeight+'px';
 // }
-document.documentElement.style.fontSize = document.documentElement.clientWidth / 6.4 + 'px';
+
 var screenWidth=document.body.clientWidth;
 var screenHeight=document.body.clientHeight;
+if (screenWidth>415) {
+    screenWidth=414;
+    screenHeight=736;
+    $(".swiper-slide").addClass("forwidth");
+    $(".swiper-pagination").addClass("sliderforwidth");
+}
+document.documentElement.style.fontSize = screenWidth/ 6.4 + 'px';
+
 window.onresize=function init() {
 	screenWidth=document.body.clientWidth;
 	screenHeight=document.body.clientHeight;
+    if (screenWidth>415) {
+        screenWidth=414;
+        screenHeight=736;
+        $(".swiper-slide").addClass("forwidth");
+    }else{
+        $(".swiper-slide").removeClass("forwidth");
+    }   
 	for(var i=0;i<11;i++){
 		document.getElementsByClassName('swiper-slide')[i].style.width=screenWidth+'px';
 		document.getElementsByClassName('swiper-slide')[i].style.height=screenHeight+'px';
 	}
-	document.documentElement.style.fontSize = document.documentElement.clientWidth / 6.4 + 'px';
+	document.documentElement.style.fontSize = screenWidth / 6.4 + 'px';
 }
-
+window.addEventListener('orientationchange',function(){
+    history.go(0);
+});
 setInterval(function () {
+    screenHeight=document.body.clientHeight;
     var height= $("#slide1").offset().top;
-    
+    console.log(height);
     if(height==(-screenHeight)){
 		$("#playintwo").addClass("fadeInDownShort");
 	}
